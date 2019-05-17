84c95cc3a7da0508bbb0b22c79a1a9b1ee65ec6f
root <pierocann93@hotmail.it>
2019-05-17 18:00:49 +0200

    put probes to false as default

--- push-to-helm.sh
+++ push-to-helm.sh
@@ -6,14 +6,17 @@ mv -f $1-0.1.0.tgz $1
 
 helm repo index . --url https://petercahn.github.io/helm-charts/
 
-
 git add --all
 git commit -a -m "$2"
 git push
 
 helm repo update
 
-cat index.yaml
-curl https://petercahn.github.io/helm-charts/index.yaml
-
+differences=$(diff <(curl https://petercahn.github.io/helm-charts/index.yaml) index.yaml)
+if [ -z "$differences" ] ; then
+	echo "File index.yaml is synced"
+else
+	echo "File index.yaml is not in sync"
+	echo $differences
+fi
 
