9bc0d3e075b8bb569545b85b2ee852308347ae0f
root <pierocann93@hotmail.it>
2019-05-17 15:24:38 +0200

    change index

---
+++ push-to-helm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+helm package $1
+
+mv -f $1-0.1.0.tgz $1
+
+helm repo index . --url https://petercahn.github.io/helm-charts/
+
+cat index.yaml
+
+git add --all
+git commit -a -m "change index"
+git push
+
+curl https://petercahn.github.io/helm-charts/
+
+
